c10305a51a0f81fd08458e4ceab2d410082ac9c6
= <androsv.developer@gmail.com>
2023-04-12 20:10:20 -0500

    feat: fixing images

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "image.tmdb.org",
+        hostname: "**",
         port: "",
-        pathname: "/**",
+        pathname: "**",
       },
     ],
   },
